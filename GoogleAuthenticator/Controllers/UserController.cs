e30921c9690c899908a10ce14992390ecd4279f2
MatijaSabo <matijasaboli37@gmail.com>
2017-10-03 14:35:55 +0200

    Završen projekt

---
+++ GoogleAuthenticator/Controllers/UserController.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using GoogleAuthenticator.Models;
+using Google.Authenticator;
+
+namespace GoogleAuthenticator.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "PrivateKey,KeyFlag")]User user)
+        {
+            bool Status = false;
+            string message = "";
+
+            #region Validacija
+            if (ModelState.IsValid)
+            {
+
+                bool flag = UserNameExist(user.UserName);
+
+                if (flag)
+                {
+                    message = "Username already exist";
+                }
+                else
+                {
+                    #region Password hash
+                    user.Password = Crypto.Hash(user.Password);
+                    user.ComfirmPassword = Crypto.Hash(user.ComfirmPassword);
+                    #endregion
+
+                    #region Google key
+                    user.KeyFlag = false;
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(user.UserName).Append(user.Email);
+
+                    user.PrivateKey = Crypto.Hash(sb.ToString());
+                    #endregion
+
+                    #region Database save
+                    using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                    {
+                        dc.Users.Add(user);
+                        dc.SaveChanges();
+
+                        message = "Registration successfully done.";
+                        Status = true;
+                    }
+                    #endregion
+                }
+            }
+            else
+            {
+                message = "Invalid request";
+            }
+            #endregion
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+
+            return View(user);
+        }
+
+        [NonAction]
+        public bool UserNameExist(String user)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.UserName == user).FirstOrDefault();
+                return v == null ? false : true;
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin user)
+        {
+            string message = "";
+            bool status = false;
+            bool show_key = true;
+
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.UserName == user.UserName).FirstOrDefault();
+                if(v != null)
+                {
+                    if(String.Compare(Crypto.Hash(user.Password), v.Password) == 0)
+                    {
+                        status = true;
+                        Session["username"] = user.UserName;
+
+                        string key = v.PrivateKey;
+                        Session["user_key"] = key;
+
+                        if (v.KeyFlag == true)
+                        {
+                            show_key = false;
+                        }
+
+                        TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+                        var SetupInfo = tfa.GenerateSetupCode("SI Projekt", user.UserName, v.PrivateKey, 300, 300);
+                        ViewBag.BarcodeImageUrl = SetupInfo.QrCodeSetupImageUrl;
+                    }
+                    else
+                    {
+                        message = "Invalid Password!";
+                        ViewBag.FailLogin = true;
+                    }
+                } 
+                else
+                {
+                    message = "Invalid Email address!";
+                    ViewBag.FailLogin = true;
+                }
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = status;
+            ViewBag.QR_show = show_key;
+
+            Session["show_key"] = show_key;
+
+            return View();
+        }
+
+        public ActionResult Verify2FA()
+        {
+            var token = Request["passcode"];
+            var key = Session["user_key"];
+            var user = Session["username"];
+            var show = Session["show_key"];
+
+            TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+            bool isValid = tfa.ValidateTwoFactorPIN(key.ToString(), token);
+
+            if (isValid)
+            {
+                Session["isValid2FA"] = true;
+
+                if((Boolean) show == true)
+                {
+                    using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                    {
+
+                        dc.Configuration.ValidateOnSaveEnabled = false;
+
+                        var v = dc.Users.Where(a => a.UserName == user.ToString()).FirstOrDefault();
+
+                        if (v != null)
+                        {
+                            v.KeyFlag = true;
+                            dc.SaveChanges();
+                        }
+
+                    }
+                }
+
+                return RedirectToAction("ProfilePage", "User");
+            }
+
+            ViewBag.Message = "2FA Failed";
+            return RedirectToAction("Login", "User");
+        }
+
+
+        public ActionResult ProfilePage()
+        {
+            if (Session["username"] == null || Session["isValid2FA"] == null || !(bool)Session["isValid2Fa"])
+            {
+                return RedirectToAction("Login");
+            }
+
+            ViewBag.Message = Session["username"].ToString();
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["username"] = null;
+            Session["isValid2FA"] = null;
+            Session["user_key"] = null;
+            Session["show_key"] = null;
+
+            ViewBag.Status = false;
+            ViewBag.Message = "Logout success";
+            return RedirectToAction("Login", "User");
+        }
+    }
+}
---
+++ GoogleAuthenticator/Models/Extended/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GoogleAuthenticator.Models
+{
+    [MetadataType(typeof(UserMetaData))]
+    public partial class User
+    {
+        public string ComfirmPassword { get; set; }
+    }
+
+    public class UserMetaData
+    {
+        [Display(Name = "First name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name is requred")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name is requred")]
+        public string LastName { get; set; }
+
+        [Display(Name = "User name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "User name is requred")]
+        public string UserName { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is requred")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Minimum 6 characters required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Comfirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm password and password do not match")]
+        public string ComfirmPassword { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is requred")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
